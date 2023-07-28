cb2d632b192a0988118da577e8627a3a9294910b
francisco espeche <99774117+pancho3025@users.noreply.github.com>
2023-07-28 19:03:32 -0300

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://pancho3025.github.io/memeg'
+  base: 'https://pancho3025.github.io/meme-generator'
 
 })
